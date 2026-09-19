583ada2f7fbcb4cc1607639381fbdc0b968625d6
Pu91 <puspendu1234you@gmail.com>
2026-09-19 11:31:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ if st.button("নোটস তৈরি করুন"):
             try:
                 # Groq-এর সুপারফাস্ট Llama 3 মডেল
                 response = client.chat.completions.create(
-                    model="mixtral-8x7b-32768",
+                    model="openai/gpt-oss-20b",
                     messages=[
                         {"role": "user", "content": prompt}
                     ]
